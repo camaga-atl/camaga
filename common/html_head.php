1211653a7fafa0380ccbacff38520a13c2a2e4e1
Srinivasan Ramaswamy <sramaswamy2050@gmail.com>
2016-05-07 07:59:20 -0400

    Added title + description to head

--- common/html_head.php
+++ common/html_head.php
@@ -1,5 +1,7 @@
 	<head>
-		<meta charset="UTF-8"> 	
+		<meta charset="UTF-8">
+                <title>CAMAGA</title>
+                <meta name="description" content="Carnatic Music Association of Georgia, Atlanta">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<base href="/camaga/">
                 <link rel="icon" href="img/favicon/favicon-16.png" sizes="16x16" type="image/png">  
