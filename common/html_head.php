d31693d12a82fee6da015a03d6adafaa4c50e631
Srinivasan Ramaswamy <sramaswamy2050@gmail.com>
2020-03-27 11:26:41 -0400

    camaga.org migrated from GoDaddy to DreamHost; http is redirected to https always

--- common/html_head.php
+++ common/html_head.php
@@ -8,16 +8,16 @@
    <link rel="stylesheet" href="css/form.css">
    <link rel="stylesheet" href="thirdparty/lightbox2/lightbox2-master/dist/css/lightbox.min.css" />
    <link rel="stylesheet" href="css/jquery-ui-timepicker-addon.css">
-   <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/ui-lightness/jquery-ui.min.css">
-   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+   <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/ui-lightness/jquery-ui.min.css">
+   <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
    <link rel="stylesheet" href="css/style.css" />
    <link rel="stylesheet" href="css/web-grid.css" />
    <link rel="stylesheet" href="thirdparty/unitegallery/unitegallery-master/package/unitegallery/css/unite-gallery.css" />
-   <link rel="stylesheet" href="thirdparty/unitegallery/unitegallery-master/package/unitegallery/themes/video/skin-right-thumb.css" />
-   <script src="http://ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.0.6-development-only.js"></script>
-   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-   <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+   <link rel="stylesheet" href="thirdparty/unitegallery/unitegallery-master/package/unitegallery/themes/video/skin-right-thumb.css" />   
+   <script src="https://ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.0.6-development-only.js"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
    <script src="js/superlink.jquery.js"></script>
    <title><?php echo $title; ?></title>
    <meta type="description" content="<?php echo $description; ?>">
