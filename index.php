0256eb368c0a3cf3a2d2b18a1ed67b552a2b1a2e
Srinivasan Ramaswamy <sramaswamy2050@gmail.com>
2021-02-12 09:52:27 -0500

    facebook live for 2021 composers day

--- index.php
+++ index.php
@@ -13,145 +13,27 @@
       <div class="wrapper">
          <?php
             include(CAMAGA_ROOT . "common/site_header.php");
-            ?>
+        ?>
         <div id="content">
-            <h2>Spring 2020 Concerts and Events</h2>
-            CAMAGA have canceled all concerts and events scheduled for Spring 2020, due to concerns of Coronavirus.
-            As announced earlier, we will soon arrange to return the cost of tickets/donations to those 
-            who had purchased/donated for Trinity Drama. 
-            We sincerely regret any inconvenience caused.
-           
-            <!-- Trinity drama START -->                   
-            <h3>Trinity Drama - Event (scheduled for March 21, 2020) is Cancelled due to Coronavirus Concerns</h3>
+        <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v9.0" nonce="scyJkvjh">
+            </script>        
+            <h2>2021 Composers Day</h2>
             <p>
-            The Trinity Drama scheduled to be held on March 21, 2020, Saturday has been canceled due to concerns of Coronavirus.
-            We regret any inconvenience caused to you. On behalf of CAMAGA, we thank each of you for purchasing tickets and 
-            supporting the event. We also sincerely thank our donors for their support for this event. We will soon arrange 
-            to return the cost of tickets to those who had purchased tickets
+            During the year 2020, because of COVID-19, CAMAGA did not hold any in-person concerts or events, with the 
+            exception of the Great Composers' Day.  However, this did not deter the Atlanta music community of 
+            rasikas and performers from continuing with musical activities.  In keeping with this spirit, in 2021, 
+            CAMAGA will host the annual Great Composers' day. The event will be an online, virtual event and unlike in 
+            prior years, the event will be much smaller in duration. The event will consist of singing the five 
+            Pancharathna kritis followed by select Utsava Sampradaya Kritis. Each of these kritis is being by one 
+            of our local musicians. Camaga thanks all the artists for their participation.
+            </p> 
+            <p>
+            You will see a <Watch Live> button that you can click to join, closer to Sat Feb 13 10 AM EST. Please do 
+            join, listen to the singing and show your appreciation of our local artists.   
             </p>
-            <p><span style="color:#00868B;">CAMAGA committee</span></p>
-            <!-- Trinity Drama End -->
- 
-            <!-- upcoming concerts START  --
-            <h1><?php echo $pageTitle; ?></h1>
-            -->
-            <?php
-               include( CAMAGA_ROOT . "concerts/upcoming.php");
-               ?>                                         
-            <!-- upcoming concerts END -->
-            <h2>CAMAGA in Pictures</h2>
-            <div id="gallery" >
-               <img src="data/concerts/201909071600/thumbs/001.jpg"
-                   data-image="data/concerts/201909071600/001.jpg"
-                   data-description="Sep 7, 2019 - K.V.Prasad, A.Kanyakumari and Vittal Rangan" />
-               <img src="data/concerts/201909071600/thumbs/002.jpg"
-                   data-image="data/concerts/201909071600/002.jpg"
-                   data-description="Sep 7, 2019 - A.Kanyakumari"/>
-               <img src="data/concerts/201909071600/thumbs/003.jpg"
-                   data-image="data/concerts/201909071600/003.jpg"
-                   data-description="Sep 7, 2019 - Vittal Rangan" />
-               <img src="data/concerts/201909071600/thumbs/004.jpg"
-                   data-image="data/concerts/201909071600/004.jpg"
-                   data-description="Sep 7, 2019 - K.V.Prasad" />
-               <img src="data/concerts/201909071600/thumbs/005.jpg"
-                   data-image="data/concerts/201909071600/005.jpg"
-                   data-description="Sep 7, 2019 - K.V.Prasad, A.Kanyakumari and Vittal Rangan" />
-               <img src="data/concerts/201909071600/thumbs/006.jpg"
-                   data-image="data/concerts/201909071600/006.jpg"
-                   data-description="Sep 7, 2019 - K.V.Prasad, A.Kanyakumari and Vittal Rangan" />
-               <img src="data/concerts/201905051630/thumbs/001.jpg"
-                   data-image="data/concerts/201905051630/001.jpg"
-                   data-description="May 5, 2019 - Srivani Yella" />
-               <img src="data/concerts/201904131600/thumbs/002.jpg"
-                   data-image="data/concerts/201904131600/002.jpg"
-                   data-description="Apr 13, 2019 - Amrutha Venkatesh" />
-               <img src="data/concerts/201904271600/thumbs/003.jpg"
-                   data-image="data/concerts/201904271600/003.jpg"
-                   data-description="Apr 27, 2019 - Ranjini Ramakrishnan" />
-               <img src="data/concerts/201903231600/thumbs/002.jpg"
-                   data-image="data/concerts/201903231600/002.jpg"
-                   data-description="Mar 23, 2019 - Thanjavur Murugaboopathy, Sherthala K.N.Renganatha Sarma and B.U.Ganesh Prasad" />
-               <img src="data/concerts/201905051630/thumbs/003.jpg"
-                   data-image="data/concerts/201905051630/003.jpg"
-                   data-description="May 5, 2019 - Ram Sriram" />
-               <img src="data/concerts/201904271600/thumbs/006.jpg"
-                   data-image="data/concerts/201904271600/006.jpg"
-                   data-description="Apr 27, 2019 - Vijayalakshmi Subramaniam" />
-               <img src="data/concerts/201905051630/thumbs/004.jpg"
-                   data-image="data/concerts/201905051630/004.jpg"
-                   data-description="May 5, 2019 - Srivani Yella" />
-               <img src="data/concerts/201904271600/thumbs/002.jpg"
-                   data-image="data/concerts/201904271600/002.jpg"
-                   data-description="Apr 27, 2019 - Vijayalakshmi Subramaniam" />
-               <img src="data/concerts/201904131600/thumbs/006.jpg"
-                   data-image="data/concerts/201904131600/006.jpg"
-                   data-description="Apr 13, 2019 - Arjun Ganesh" />
-               <img src="data/concerts/201904061600/thumbs/002.jpg"
-                   data-image="data/concerts/201904061600/002.jpg"
-                   data-description="Apr 6, 2019 - V.K.Manimaran" />
-               <img src="data/concerts/201904271600/thumbs/004.jpg"
-                   data-image="data/concerts/201904271600/004.jpg"
-                   data-description="Apr 27, 2019 - Karthik Narayanan" />
-               <img src="data/concerts/201904131600/thumbs/005.jpg"
-                   data-image="data/concerts/201904131600/005.jpg"
-                   data-description="Apr 13, 2019 - Arjun Ganesh" />
-               <img src="data/concerts/201905051630/thumbs/002.jpg"
-                   data-image="data/concerts/201905051630/002.jpg"
-                   data-description="May 5, 2019 - Ram Sriram & Srivani Yella" />
-               <img src="data/concerts/201904061600/thumbs/004.jpg"
-                   data-image="data/concerts/201904061600/004.jpg"
-                   data-description="Apr 6, 2019 - Poongulam Subramaniam" />
-               <img src="data/concerts/201904271600/thumbs/005.jpg"
-                   data-image="data/concerts/201904271600/005.jpg"
-                   data-description="Apr 27, 2019 - Vijayalakshmi Subramaniam" />
-               <img src="data/concerts/201904131600/thumbs/004.jpg"
-                   data-image="data/concerts/201904131600/004.jpg"
-                   data-description="Apr 13, 2019 - Rajeev Mukundan" />
-               <img src="data/concerts/201903231600/thumbs/001.jpg"
-                   data-image="data/concerts/201903231600/001.jpg"
-                   data-description="Mar 23, 2019 - Thanjavur Murugaboopathy, Sherthala K.N.Renganatha Sarma and B.U.Ganesh Prasad" />
-               <img src="data/concerts/201904061600/thumbs/001.jpg"
-                   data-image="data/concerts/201904061600/001.jpg"
-                   data-description="Apr 6, 2019 - Poongulam Subramaniam, V.K.Manimaran and B.Raghavendra Rao" />
-               <img src="data/concerts/201904271600/thumbs/007.jpg"
-                   data-image="data/concerts/201904271600/007.jpg"
-                   data-description="Apr 27, 2019 - Karthik Narayanan, Vijayalakshmi Subramaniam and Ranjini Ramakrishnan" />
-               <img src="data/concerts/201904131600/thumbs/001.jpg"
-                   data-image="data/concerts/201904131600/001.jpg"
-                   data-description="Apr 13, 2019 - Arjun Ganesh, Amrutha Venkatesh and Rajeev Mukundan" />
-               <img src="data/concerts/201904131600/thumbs/003.jpg"
-                   data-image="data/concerts/201904131600/003.jpg"
-                   data-description="Apr 13, 2019 - Arjun Ganesh" />
-               <img src="data/concerts/201904271600/thumbs/001.jpg"
-                   data-image="data/concerts/201904271600/001.jpg"
-                   data-description="Apr 27, 2019 - Karthik Narayanan, Vijayalakshmi Subramaniam and Ranjini Ramakrishnan" />
-               <img src="data/concerts/201904061600/thumbs/003.jpg"
-                   data-image="data/concerts/201904061600/003.jpg"
-                   data-description="Apr 6, 2019 - B.Raghavendra Rao" />
+            <div class="camaga-fb-post" >
+               <div class="fb-post" data-href="https://www.facebook.com/camaga.org/posts/2986792304882717" data-width="750" data-show-text="true"><blockquote cite="https://www.facebook.com/camaga.org/posts/2986792304882717" class="fb-xfbml-parse-ignore"><p>Annual Composers Day - Virtual Broadcast on February 13, 2021 at 10:00 AM Eastern Time Zone</p>Posted by <a href="https://www.facebook.com/camaga.org/">Camaga</a> on&nbsp;<a href="https://www.facebook.com/camaga.org/posts/2986792304882717">Tuesday, February 9, 2021</a></blockquote></div>
             </div>
-            <br/></br/>
-         </div>
-         <div class="push"></div>
-      </div>
-      <?php
-         include(CAMAGA_ROOT . "common/site_footer.php");
-         ?>
-      <script src='thirdparty/unitegallery/unitegallery-master/package/unitegallery/js/unitegallery.min.js'></script>
-      <script src='thirdparty/unitegallery/unitegallery-master/package/unitegallery/themes/compact/ug-theme-compact.js'></script>
-      <script src='thirdparty/unitegallery/unitegallery-master/package/unitegallery/themes/video/ug-theme-video.js'></script>
-      <script type="text/javascript">
-         jQuery(document).ready(function(){
-         	jQuery("#gallery").unitegallery({
-         		gallery_theme: "compact",
-         		gallery_min_width: 150,
-         		slider_scale_mode: "down",
-         		theme_hide_panel_under_width: 4000,
-         		slider_enable_text_panel: true
-         	});
-            jQuery("#trinity-drama-gallery").unitegallery({
-                gallery_theme: "video"
-            });
-         });
-      </script>
    </body>
 </html>
