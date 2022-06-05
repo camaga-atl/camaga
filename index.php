e5ba816925986816df34d430845e7ae52db47c8d
Srinivasan Ramaswamy <SRamaswamy2050@gmail.com>
2022-06-05 15:15:27 -0400

    2022 oothukadu venkata subbaiyer celebration

--- index.php
+++ index.php
@@ -16,64 +16,17 @@ include(CAMAGA_ROOT . "common/html_head.php");
       include(CAMAGA_ROOT . "common/site_header.php");
       ?>
       <div id="content">
-         <!-- upcoming concerts START -->
-         <h2>Concert at the Seaside Temple - Online event</h2>
-         <h4>Online on June 18,19 and 20, 2021 (Friday through Sunday)</h4>
-         <p>
-            CAMAGA is presenting an online event - A vocal music performance titled "Concert at the Seaside Temple” by the following artists
-         </p>
-         <div>
-            <span class="artist">
-               <a target="_blank" href="https://www.google.com/search?q=Gayathri Venkataraghavan">
-                  <strong>Gayathri Venkataraghavan</strong>
-               </a>
-            </span>
-            <span class="instrument">
-               Vocal </span>
-         </div>
-         <div>
-            <span class="artist">
-               <a target="_blank" href="https://www.google.com/search?q=Mysore Srikanth">
-                  <strong>Mysore Srikanth</strong>
-               </a>
-            </span>
-            <span class="instrument">
-               Violin </span>
-         </div>
-         <div>
-            <span class="artist">
-               <a target="_blank" href="https://www.google.com/search?q=B.Sivaraman">
-                  <strong>B.Sivaraman</strong>
-               </a>
-            </span>
-            <span class="instrument">
-               Mridangam </span>
-         </div>
-         <div>
-            <span class="artist">
-               <a target="_blank" href="https://www.google.com/search?q=K.V.Gopalakrishnan">
-                  <strong>K.V.Gopalakrishnan</strong>
-               </a>
-            </span>
-            <span class="instrument">
-               Kanjira </span>
-         </div>
-         <br/>
-         <div>
-            <img src="/events/2021/concert-at-the-seaside-temple/1.jpg" width="360px" />
-         </div>
-         <h4>How to view?</h4>
-         <p>
-            The concert will be available for online viewing and listening, tentatively on June 18, 19, and 20 (Friday through Sunday).
-            The concert will be available ONLY for a maximum of 80 hours during the three days and during these days and hours, viewers can stop and continue as they prefer.
-            This event available ONLY to those who were members of CAMAGA as of the year 2019.
-         </p>
-         <p>
-            A few days before June 15, we will share the access information and other details.
-
-            Please make sure you listen to the concert during the dates and period it is made available. If you missed it, CAMAGA will not be able to either extend the hours or change the dates. We regret that this is the stipulation per the contract, and, as an organization, CAMAGA must abide by it.
-         </p>
+         <!-- upcoming concerts START  -->
+         <?php
+            include( CAMAGA_ROOT . "concerts/upcoming.php");
+         ?>
          <!-- upcoming concerts END -->
+         <br/>
+         <br/>
+         <p>
+            <i>* membership</i><br/>
+            Membership is open for 2022. Please note the <a href="member">new membership rates</a>
+         </p>   
       </div>
    </div>
 </body>
